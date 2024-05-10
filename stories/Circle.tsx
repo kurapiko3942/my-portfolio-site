0746dea0a3c2ddaefaa19ec7d4139486599f7671
Kouki Kanayama <kouki@kurapiko.com>
2024-05-10 12:47:04 +0900

    ログインフォームとテーブルUIの作成、SUPABASEとの連携

---
+++ stories/Circle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Circle = () => {
+  return (
+    <div className="bg-orange-500 w-14 h-14 p-2 rounded-full">
+        <h1>Circle</h1>
+    </div>
+  )
+}
+
+export default Circle
